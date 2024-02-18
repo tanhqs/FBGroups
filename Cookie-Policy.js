3d2d8e218f1aadb77d94e648730fe92cbed54825
tanhqs <55470051+tanhqs@users.noreply.github.com>
2024-02-18 18:53:32 +0700

    Add files via upload

---
+++ Cookie-Policy.js
@@ -0,0 +1,33 @@
+var content = `
+	<p><img src="/info/Cookie-Policy.jpg" alt="Cookie Policy" title="Terms Conditions"></p>
+	<br>
+	<br>
+	<h1>Cookie policy</h1><br>
+	<p>This cookie policy (&quot;Policy&quot;) describes what cookies are and how and they&#039;re being used by the <a target="_blank" rel="nofollow" href="https://8w8.info">localhost</a> website (&quot;Website&quot; or &quot;Service&quot;) and any of its related products and services (collectively, &quot;Services&quot;). This Policy is a legally binding agreement between you (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;) and this Website operator (&quot;Operator&quot;, &quot;we&quot;, &quot;us&quot; or &quot;our&quot;). You should read this Policy so you can understand the types of cookies we use, the information we collect using cookies and how that information is used. It also describes the choices available to you regarding accepting or declining the use of cookies. For further information on how we use, store and keep your personal data secure, see our <a target="_blank" rel="nofollow" href="https://8w8.info/privacy-policy">privacy policy</a>. </p><br>
+	<h2>What are cookies?</h2><br>
+	<p>Cookies are small pieces of data stored in text files that are saved on your computer or other devices when websites are loaded in a browser. They are widely used to remember you and your preferences, either for a single visit (through a &quot;session cookie&quot;) or for multiple repeat visits (using a &quot;persistent cookie&quot;).</p><br>
+	<p>Session cookies are temporary cookies that are used during the course of your visit to the Website, and they expire when you close the web browser.</p><br>
+	<p>Persistent cookies are used to remember your preferences within our Website and remain on your desktop or mobile device even after you close your browser or restart your computer. They ensure a consistent and efficient experience for you while visiting the Website and Services.</p><br>
+	<p>Cookies may be set by the Website (&quot;first-party cookies&quot;), or by third parties, such as those who serve content or provide advertising or analytics services on the Website (&quot;third party cookies&quot;). These third parties can recognize you when you visit our website and also when you visit certain other websites. <a target="_blank" href="https://www.websitepolicies.com/blog/cookies">Click here</a> to learn more about cookies and how they work.</p><br>
+	<h2>What type of cookies do we use?</h2><br>
+	<h3>Necessary cookies</h3><br>
+	<p>Necessary cookies allow us to offer you the best possible experience when accessing and navigating through our Website and using its features. For example, these cookies let us recognize that you have created an account and have logged into that account to access the content.</p><br>
+	<h3>Functionality cookies</h3><br>
+	<p>Functionality cookies let us operate the Website and Services in accordance with the choices you make. For example, we will recognize your username and remember how you customized the Website and Services during future visits.</p><br>
+	<h3>Analytical cookies</h3><br>
+	<p>These cookies enable us and third party services to collect aggregated data for statistical purposes on how our visitors use the Website. These cookies do not contain personal information such as names and email addresses and are used to help us improve your user experience of the Website.</p><br>
+	<h3>Social media cookies</h3><br>
+	<p>Third party cookies from social media sites (such as Facebook, Twitter, etc) let us track social network users when they visit or use the Website and Services, or share content, by using a tagging mechanism provided by those social networks.</p><br>
+	<p>These cookies are also used for event tracking and remarketing purposes. Any data collected with these tags will be used in accordance with our and social networks’ privacy policies. We will not collect or share any personally identifiable information from the user.</p><br>
+	<h2>What are your cookie options?</h2><br>
+	<p>If you don't like the idea of cookies or certain types of cookies, you can change your browser's settings to delete cookies that have already been set and to not accept new cookies. To learn more about how to do this, visit <a target="_blank" href="https://www.internetcookies.com" rel="noopener">internetcookies.com</a></p><br>
+	<p>Please note, however, that if you delete cookies or do not accept them, you might not be able to use all of the features the Website and Services offer.</p><br>
+	<h2>Changes and amendments</h2><br>
+	<p>We reserve the right to modify this Policy or its terms relating to the Website and Services at any time, effective upon posting of an updated version of this Policy on the Website. When we do, we will revise the updated date at the bottom of this page. Continued use of the Website and Services after any such changes shall constitute your consent to such changes.</p><br>
+	<h2>Acceptance of this policy</h2><br>
+	<p>You acknowledge that you have read this Policy and agree to all its terms and conditions. By accessing and using the Website and Services you agree to be bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not authorized to access or use the Website and Services.</p><br>
+	<h2>Contacting us</h2><br>
+	<p>If you would like to contact us to understand more about this Policy or wish to contact us concerning any matter relating to our use of cookies, you may send an email to admin.</p><br>
+	<p>This document was last updated on July 3, 2021</p>
+`;
+document.write(content);
---
+++ Refund-and-Returns-Policy.js
@@ -0,0 +1,68 @@
+var content = `
+	<img title="Refund and Returns Policy" src="/info/Refund-and-Returns-Policy.jpg" alt="Refund and Returns Policy" title="Terms Conditions">
+	<br>
+	<br>
+	<h3>Overview</h3>
+	Our refund and returns policy lasts 30 days. If 30 days have passed since your purchase, we can’t offer you a full refund or exchange.
+	<br>
+	To be eligible for a return, your item must be unused and in the same condition that you received it. It must also be in the original packaging.
+	<br>
+	Several types of goods are exempt from being returned. Perishable goods such as food, flowers, newspapers or magazines cannot be returned. We also do not accept products that are intimate or sanitary goods, hazardous materials, or flammable liquids or gases.
+	<br>
+	<b>Additional non-returnable items:</b>
+	<ul>
+		<li>Gift cards</li>
+		<li>Downloadable software/online products</li>
+		<li>Some health and personal care items</li>
+	</ul>
+	To complete your return, we require a receipt or proof of purchase.
+	<br>
+	Please do not send your purchase back to the manufacturer.
+	<br>
+	<b>There are certain situations where only partial refunds are granted:</b>
+	<ul>
+		<li>Book with obvious signs of use</li>
+		<li>CD, DVD, VHS tape, software, video game, cassette tape, or vinyl record that has been opened.</li>
+		<li>Any item not in its original condition, is damaged or missing parts for reasons not due to our error.</li>
+		<li>Any item that is returned more than 30 days after delivery</li>
+	</ul>
+	<br>
+	<h3>Refunds</h3>
+	Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.
+	<br>
+	If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.
+	<br>
+	<b>Late or missing refunds</b>
+	<br>
+	If you haven’t received a refund yet, first check your bank account again.
+	<br>
+	Then contact your credit card company, it may take some time before your refund is officially posted.
+	<br>
+	Next contact your bank. There is often some processing time before a refund is posted.
+	<br>
+	If you’ve done all of this and you still have not received your refund yet, please contact us at admin@localhost.
+	<br>
+	<b>Sale items</b>
+	<br>
+	Only regular priced items may be refunded. Sale items cannot be refunded.
+	<br>
+	<h3>Exchanges</h3>
+	We only replace items if they are defective or damaged. If you need to exchange it for the same item, send us an email at admin@localhost and send your item to: See contact page.
+	<h3>Gifts</h3>
+	If the item was marked as a gift when purchased and shipped directly to you, you’ll receive a gift credit for the value of your return. Once the returned item is received, a gift certificate will be mailed to you.
+	<br>
+	If the item wasn’t marked as a gift when purchased, or the gift giver had the order shipped to themselves to give to you later, we will send a refund to the gift giver and they will find out about your return.
+	<br>
+	<h3>Shipping returns</h3>
+	To return your product, you should mail your product to: See contact page.
+	<br>
+	You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund.
+	<br>
+	Depending on where you live, the time it may take for your exchanged product to reach you may vary.
+	<br>
+	If you are returning more expensive items, you may consider using a trackable shipping service or purchasing shipping insurance. We don’t guarantee that we will receive your returned item.
+	<br>
+	<h3>Need help?</h3>
+	Contact us at admin@localhost for questions related to refunds and returns.
+`;
+document.write(content);
